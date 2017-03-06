912962d87c4c019d9439f08b70de0639fe031c0c
Danny Bondarenko <Danylo_Bondarenko@epam.com>
2017-03-06 05:06:34 +0200

    Added nunit test

---
+++ AutoTestsProject/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AutoTestsProject
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test]
+        public void NUnitSampleTest1()
+        {
+            Assert.Pass("This is sample nunit test");
+        }
+    }
+}
